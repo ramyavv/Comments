5640cc0b100530ebb468136467bc83354457b1d2
ramyavv <ramya.vemuganti@gmail.com>
2014-07-28 13:58:03 -0500

    refactor

--- app.js
+++ app.js
@@ -2,9 +2,7 @@ var main = function() {
     "use strict"
 var addComment = function() {
 
-    $(".comment-input button").on("click", function(event) {
-
-    	if($(".comment-input input").val() !== "") {
+	if($(".comment-input input").val() !== "") {
 
         var new_comment = $("<p>").text($(".comment-input input").val());
 
@@ -17,23 +15,24 @@ var addComment = function() {
         $(".comment-input input").val("");
               }
 };
+
+
+
+
+
+
+
+    $(".comment-input button").on("click", function(event) {
+
+    	addComment();
     });
 
     $(".comment-input input").on("keypress", function(event) {
      
          if(event.keyCode == 13){
-    	if($(".comment-input input").val() !== "") {
-
-        var new_comment = $("<p>").text($(".comment-input input").val());
-
-        new_comment.hide();
-
-        $(".comments").append(new_comment);
-        
-        new_comment.fadeIn();
-        $(".comment-input input").val("");
-              }
-          }
+    	  
+    	  addComment();
+    	}
     });
 };
 

d0c03044d34709e9fb15a5f8a1c5e3de86738008
ramyavv <ramya.vemuganti@gmail.com>
2014-07-28 13:29:34 -0500

    Don't allow empty comment field.

--- app.js
+++ app.js
@@ -3,10 +3,13 @@ var main = function() {
 
     $(".comment-input button").on("click", function(event) {
 
+    	if($(".comment-input input").val() !== "") {
+
         var new_comment = $("<p>").text($(".comment-input input").val());
 
         $(".comments").append(new_comment);
         $(".comment-input input").val("");
+              }
     });
 };
 

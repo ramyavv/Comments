f3a18ea785f09f3d3c0048e566c702cc38b8e13a
ramyavv <ramya.vemuganti@gmail.com>
2014-07-28 13:50:52 -0500

    Add an effect to fade in after hiding the element

--- app.js
+++ app.js
@@ -7,8 +7,28 @@ var main = function() {
 
         var new_comment = $("<p>").text($(".comment-input input").val());
 
-         if(event.keyCode == 13){
+        new_comment.hide();
+
         $(".comments").append(new_comment);
+
+        new_comment.fadeIn();
+        
+        $(".comment-input input").val("");
+              }
+    });
+
+    $(".comment-input input").on("keypress", function(event) {
+     
+         if(event.keyCode == 13){
+    	if($(".comment-input input").val() !== "") {
+
+        var new_comment = $("<p>").text($(".comment-input input").val());
+
+        new_comment.hide();
+
+        $(".comments").append(new_comment);
+        
+        new_comment.fadeIn();
         $(".comment-input input").val("");
               }
           }
